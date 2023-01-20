49f4b0886669275a2f1e463d91ce464d382d9dcc
Matthias Rozanski <matthiasrozanski@gmail.com>
2023-01-20 19:45:39 +0100

    now uses type alias instead of template parameter (single point of definition)

--- src/device_manager_base.h
+++ src/device_manager_base.h
@@ -13,8 +13,8 @@ namespace vaatv::internal {
     class device_manager_base {
     public:
         using device_type = device_t;
-        using container_type = std::vector<device_t>;
-        using ref_container_type = std::vector<std::reference_wrapper<device_t>>;
+        using container_type = std::vector<device_type>;
+        using ref_container_type = std::vector<std::reference_wrapper<device_type>>;
 
     protected:
         // region ops for all concept::device types
@@ -35,13 +35,13 @@ namespace vaatv::internal {
         // endregion
 
         // region ops for concept::user_device types
-        void login_devices(const auto &error_callback) noexcept requires concepts::user_device<device_t> {
+        void login_devices(const auto &error_callback) noexcept requires concepts::user_device<device_type> {
             for (auto &device : m_devices) {
                 if (!device.login()) { error_callback(device); }
             }
         }
 
-        void logout_devices() noexcept requires concepts::user_device<device_t> {
+        void logout_devices() noexcept requires concepts::user_device<device_type> {
             for (auto &device : m_devices) { device.logout(); }
         }
         // endregion
